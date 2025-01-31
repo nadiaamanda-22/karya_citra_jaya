134b141b96752f03aa4c1142073b7f4897ad48b7
Riskapravita <riskapravita6@gmail.com>
2025-01-31 15:24:22 +0700

    membuat hal tambah cust

---
+++ application/views/administrasi/customer_add.php
@@ -0,0 +1,28 @@
+ <!-- Begin Page Content -->
+ <div class="container-fluid">
+     <div class="card shadow mb-4">
+         <div class="card-header py-3">
+             <p class="m-0">Tambah Customer</p>
+         </div>
+         <div class="card-body">
+             <div class="row">
+                 <div class="col-md-9">
+                     <form action="<?= base_url('customer/addData') ?>" method="post">
+                         <div class="mb-3">
+                             <label for="customer" class="form-label">Customer *</label>
+                             <input type="text" class="form-control" id="customer" name="nama_customer">
+                             <?php if ($this->session->flashdata('message', 'error')): ?>
+                                 <small class="text-danger">Customer harus diisi!</small>
+                             <?php endif; ?>
+                         </div>
+                         <div class="mb-5">
+                             <small>[*] Kolom harus diisi</small>
+                         </div>
+                         <a href="<?= base_url('customer') ?>" class="btn btn-danger">Batal</a>
+                         <button type="submit" class="btn btn-primary">Simpan</button>
+                     </form>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
