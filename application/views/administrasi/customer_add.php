77e448c0773f43045767dcb11cfa6a2c5fcb6b16
Riskapravita <riskapravita6@gmail.com>
2025-02-07 15:53:02 +0700

    edit bagian no hp dan alamat

--- application/views/administrasi/customer_add.php
+++ application/views/administrasi/customer_add.php
@@ -1,5 +1,5 @@
- <!-- Begin Page Content -->
- <div class="container-fluid">
+<!-- Begin Page Content -->
+<div class="container-fluid">
      <div class="card shadow mb-4">
          <div class="card-header py-3">
              <p class="m-0">Tambah Customer</p>
@@ -9,10 +9,24 @@
                  <div class="col-md-9">
                      <form action="<?= base_url('customer/addData') ?>" method="post">
                          <div class="mb-3">
-                             <label for="customer" class="form-label">Customer *</label>
+                             <label for="customer" class="form-label">Nama *</label>
                              <input type="text" class="form-control" id="customer" name="nama_customer">
                              <?php if ($this->session->flashdata('message', 'error')): ?>
-                                 <small class="text-danger">Customer harus diisi!</small>
+                                 <small class="text-danger">Nama harus diisi!</small>
+                             <?php endif; ?>
+                         </div>
+                         <div class="mb-3">
+                             <label for="no_hp" class="form-label">No Handphone *</label>
+                             <input type="text" class="form-control" id="customer" name="no_hp">
+                             <?php if ($this->session->flashdata('message', 'error')): ?>
+                                 <small class="text-danger">No Handphone harus diisi!</small>
+                             <?php endif; ?>
+                         </div>
+                         <div class="mb-3">
+                             <label for="alamat" class="form-label">Alamat *</label>
+                             <input type="text" class="form-control" id="customer" name="alamat">
+                             <?php if ($this->session->flashdata('message', 'error')): ?>
+                                 <small class="text-danger">Alamat harus diisi!</small>
                              <?php endif; ?>
                          </div>
                          <div class="mb-5">
